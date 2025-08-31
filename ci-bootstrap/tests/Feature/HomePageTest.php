3dd80d4c310141bc1b7b324eaaaff5e76879f227
sosilans <134475659+sosilans@users.noreply.github.com>
2025-08-30 20:52:48 -0700

    ci: run migrations and tests during bootstrap; add home page test overlay

---
+++ ci-bootstrap/tests/Feature/HomePageTest.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+it('renders home page', function () {
+    $response = $this->get('/');
+    $response->assertStatus(200);
+});
