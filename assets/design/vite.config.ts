873af7fa7d8b1bfaadabee0de88d6b80bf98e1ad
sosilans <134475659+sosilans@users.noreply.github.com>
2025-08-31 19:28:45 -0700

    ci(preview): add workflow to build design preview and set Vite base to ./

--- assets/design/vite.config.ts
+++ assets/design/vite.config.ts
@@ -1,9 +1,9 @@
-
+﻿
   import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-  export default defineConfig({
+  export default defineConfig({ `n  base: ./, 
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
