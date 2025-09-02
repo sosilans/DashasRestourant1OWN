c9997576b46958cfaf98b75bc686b69641b9594c
sosilans <134475659+sosilans@users.noreply.github.com>
2025-09-01 19:20:43 -0700

    Deploy Figma React code - Full React app with working design from localhost

--- improved-template/src/App.tsx
+++ improved-template/src/App.tsx
@@ -1,11 +1,10 @@
-import Navbar from './components/Navbar';
+import VintageHero from './components/VintageHero';
 import AboutUs from './components/AboutUs';
 import Menu from './components/Menu';
 import Specials from './components/Specials';
 import Reviews from './components/Reviews';
 import HoursLocation from './components/HoursLocation';
 import Footer from './components/Footer';
-import VintageHero from './components/VintageHero';
 
 export default function App() {
   return (
@@ -19,7 +18,6 @@ export default function App() {
       />
       
       <div className="overflow-x-hidden">
-  <Navbar />
         <VintageHero />
         <AboutUs />
         <Menu />
